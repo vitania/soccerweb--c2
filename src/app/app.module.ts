eebe761d490ff8e1ce8e30094af92cd0c6f7efee
vitania <permatasarivita80@gmail.com>
2021-10-15 14:29:30 +0700

    ini adalah project pertama githubku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageOneComponent } from './page-one/page-one.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageOneComponent
   ],
   imports: [
     BrowserModule,
